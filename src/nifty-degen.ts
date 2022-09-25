c15f2e58efa7c81cb0a693ec5c6ff9b91bab4663
NiftyAndy <andy@niftyleague.com>
2022-09-25 15:06:11 -0400

    Initial commit

---
+++ src/nifty-degen.ts
@@ -0,0 +1,89 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  NiftyDegen,
+  Approval,
+  ApprovalForAll,
+  NameUpdated,
+  OwnershipTransferred,
+  Paused,
+  Transfer,
+  Unpaused
+} from "../generated/NiftyDegen/NiftyDegen"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.MAX_SUPPLY(...)
+  // - contract.NAME_CHANGE_PRICE(...)
+  // - contract.SPECIAL_CHARACTERS(...)
+  // - contract.balanceOf(...)
+  // - contract.changeName(...)
+  // - contract.getApproved(...)
+  // - contract.getCharacterTraits(...)
+  // - contract.getNFTPrice(...)
+  // - contract.getName(...)
+  // - contract.getRemovedTraits(...)
+  // - contract.isApprovedForAll(...)
+  // - contract.isAvailableAndAllowedTrait(...)
+  // - contract.isAvailableTrait(...)
+  // - contract.isNameReserved(...)
+  // - contract.isUnique(...)
+  // - contract.name(...)
+  // - contract.owner(...)
+  // - contract.ownerOf(...)
+  // - contract.paused(...)
+  // - contract.supportsInterface(...)
+  // - contract.symbol(...)
+  // - contract.tokenURI(...)
+  // - contract.totalSupply(...)
+  // - contract.validateName(...)
+}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function handleNameUpdated(event: NameUpdated): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handlePaused(event: Paused): void {}
+
+export function handleTransfer(event: Transfer): void {}
+
+export function handleUnpaused(event: Unpaused): void {}
